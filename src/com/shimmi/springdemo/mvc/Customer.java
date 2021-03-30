d234d292a91efe210f55c8f5e0d479eab9dbefe5
Prashi <shimmikachhap@gamil.com>
2021-03-30 14:42:08 +0530

    controller-customer

--- src/com/shimmi/springdemo/mvc/Customer.java
+++ src/com/shimmi/springdemo/mvc/Customer.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Customer {
-
+//controller
 	private String firstName;
 	
 	@NotNull(message="is required")
